77db1187569a7b45fc7378f57fdf90652342f5fe
Edsonjsf <edsonjsf1990@gmail.com>
2023-02-21 16:53:17 -0400

    Implementar todos los hooks del ciclo de vida

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -1,12 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-pagina1',
   templateUrl: './pagina1.component.html',
   styleUrls: ['./pagina1.component.scss'],
 })
-export class Pagina1Component implements OnInit {
-  constructor() {}
+export class Pagina1Component
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  constructor() {
+    console.log('constructor');
+  }
 
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    // console.log("ngOnChanges");
+    return;
+  }
+
+  ngDoCheck(): void {
+    // console.log("ngDoCheck");
+    return;
+  }
+
+  ngAfterContentChecked(): void {
+    // console.log("ngAfterContentChecked");
+    return;
+  }
+
+  ngAfterViewInit(): void {
+    // console.log("ngAfterViewInit");
+    return;
+  }
+
+  ngAfterViewChecked(): void {
+    // console.log("ngAfterViewChecked");
+    return;
+  }
+
+  ngOnDestroy(): void {
+    // console.log("ngOnDestroy");
+    return;
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
 }
