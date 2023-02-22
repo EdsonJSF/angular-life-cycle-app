5777fb05f3272deb1df83b725428ef38522b7797
Edsonjsf <edsonjsf1990@gmail.com>
2023-02-22 13:27:43 -0400

    Uso de los ciclos de vida

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -10,11 +10,12 @@ import {
   OnInit,
   SimpleChanges,
 } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pagina1',
   templateUrl: './pagina1.component.html',
-  styleUrls: ['./pagina1.component.scss'],
+  styles: [],
 })
 export class Pagina1Component
   implements
@@ -27,46 +28,50 @@ export class Pagina1Component
     AfterViewChecked,
     OnDestroy
 {
+  nombre: string = 'Fernando';
+  segundos: number = 0;
+  timerSubscription!: Subscription;
+
   constructor() {
     console.log('constructor');
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    // console.log("ngOnChanges");
-    return;
+    console.log('ngOnChanges');
   }
 
   ngDoCheck(): void {
-    // console.log("ngDoCheck");
-    return;
+    // console.log('ngDoCheck');
   }
 
   ngAfterContentInit(): void {
-    // console.log("ngAfterContentInit");
-    return;
+    console.log('ngAfterContentInit');
   }
 
   ngAfterContentChecked(): void {
-    // console.log("ngAfterContentChecked");
-    return;
+    // console.log('ngAfterContentChecked');
   }
 
   ngAfterViewInit(): void {
-    // console.log("ngAfterViewInit");
-    return;
+    console.log('ngAfterViewInit');
   }
 
   ngAfterViewChecked(): void {
-    // console.log("ngAfterViewChecked");
-    return;
+    // console.log('ngAfterViewChecked');
   }
 
   ngOnDestroy(): void {
-    // console.log("ngOnDestroy");
-    return;
+    console.log('ngOnDestroy');
+    this.timerSubscription.unsubscribe();
+    console.log('timer limpiado');
   }
 
   ngOnInit(): void {
     console.log('ngOnInit');
+    this.timerSubscription = interval(1000).subscribe((i) => {
+      this.segundos = i;
+    });
   }
+
+  guardar() {}
 }
